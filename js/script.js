f544b9ecc04af72fb19f19c45a97ac256223b601
valerio <valerio.deprosperi@gmail.com>
2020-08-03 16:08:47 +0200

    nome + cognom+ colore + 19

--- js/script.js
+++ js/script.js
@@ -6,3 +6,6 @@ var colorePreferito;
 nomeUtente = prompt("come ti chiami?");
 cognomeUtente = prompt("cognome?");
 colorePreferito = prompt("il tuo colore preferito?");
+
+// nome-cognome-colore+19
+document.getElementById('nomecogn_colore').innerHTML = nomeUtente + cognomeUtente + colorePreferito + 19;
